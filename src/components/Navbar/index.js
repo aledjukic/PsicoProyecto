308d4bfde5ead4b22758a8749de08dd9b3375b7b
Alejandro Djukic <alejandrodjukic99@gmail.com>
2021-11-21 14:49:38 -0400

    Navbar y Sidebar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements';
+
+const Navbar = ()  => {
+    return (
+       <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>Name</NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="discover">Discover</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="sign-up">Sign up</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+       </>
+    )
+};
+
+export default Navbar;

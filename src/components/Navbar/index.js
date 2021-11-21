9889279c8e1dde10a2b72f7523b03be8fab7d8f4
Alejandro Djukic <alejandrodjukic99@gmail.com>
2021-11-21 18:17:40 -0400

    Hero Section

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,28 +2,28 @@ import React from 'react';
 import {FaBars} from 'react-icons/fa'
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements';
 
-const Navbar = ()  => {
+const Navbar = ({toggle})  => {
     return (
        <>
         <Nav>
             <NavbarContainer>
-                <NavLogo to='/'>Name</NavLogo>
-                <MobileIcon>
+                <NavLogo to='/'>MindSet </NavLogo>
+                <MobileIcon onClick={toggle}>
                     <FaBars />
                 </MobileIcon>
                 <NavMenu>
                     <NavItem>
-                        <NavLinks to="about">About</NavLinks>
+                        <NavLinks to="about" onClick={toggle}>About</NavLinks>
                     </NavItem>
                     <NavItem>
-                        <NavLinks to="discover">Discover</NavLinks>
+                        <NavLinks to="discover" onClick={toggle}>Discover</NavLinks>
                     </NavItem>
                     <NavItem>
-                        <NavLinks to="sign-up">Sign up</NavLinks>
+                        <NavLinks to="sign-up" onClick={toggle}>Sign up</NavLinks>
                     </NavItem>
                 </NavMenu>
                 <NavBtn>
-                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    <NavBtnLink to="/signin" onClick={toggle}>Sign In</NavBtnLink>
                 </NavBtn>
             </NavbarContainer>
         </Nav>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import {SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink,SideBtnWrap, SidebarRoute} from './SidebarElements'
 
-const Sidebar = () => {
+const Sidebar = ({isOpen, toggle}) => {
     return (
-        <SidebarContainer>
-            <Icon>
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onclick={toggle}>
                 <CloseIcon />
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to="about">About</SidebarLink>
-                    <SidebarLink to="discover">Discover</SidebarLink>
-                    <SidebarLink to="singup">Sign Up</SidebarLink>
+                    <SidebarLink to="about" onClick={toggle}>About</SidebarLink>
+                    <SidebarLink to="discover" onClick={toggle}>Discover</SidebarLink>
+                    <SidebarLink to="singup" onClick={toggle}>Sign Up</SidebarLink>
                 </SidebarMenu>
                 <SideBtnWrap>
                     <SidebarRoute to="/signin">Sign In</SidebarRoute>
